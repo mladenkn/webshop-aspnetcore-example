13f37e185cb79d96bf6bbab8865f4317a50864d1
mladen <mladen.knezovic.1993@gmail.com>
2018-10-11 02:06:59 +0200

    updated api stuff

--- WebShop.Web/Controllers/OneController.cs
+++ WebShop.Web/Controllers/OneController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using ApplicationKernel.Infrastructure;
+﻿using System.Threading.Tasks;
 using ApplicationKernel.Infrastructure.RestApi;
 using Microsoft.AspNetCore.Mvc;
-using Utilities;
-using WebShop.Baskets;
 using WebShop.UseCases;
 
 namespace WebShop.Web.Controllers
@@ -13,30 +8,16 @@ namespace WebShop.Web.Controllers
     [ApiController]
     public class OneController : ApiController
     {
+        [HttpPost]
         [Route("/discounts")]
         public Task<IActionResult> Post(AddDiscount.Request request) => Handle(request);
 
+        [HttpPost]
         [Route("/basketitems")]
         public Task<IActionResult> Post(AddBasketItem.Request request) => Handle(request);
 
+        [HttpGet]
         [Route("baskets")]
-        public async Task<IActionResult> Get(int basketId)
-        {
-            var response = await Handle(new GetBasket.Request{Id = basketId});
-            if (response is OkObjectResult result)
-            {
-                var basket = (Basket) result.Value;
-
-                basket.Items.Must().NotBeNull();
-                basket.Items.First().Discounts.Must().NotBeNull();
-                basket.Items.First().Product.Must().NotBeNull();
-
-                return SerializerOf<Basket>()
-                    .IgnoreProperty("Items.BasketItemDiscounts")
-                    .Serialize()
-                    .WrapIntoOkObjectResult();
-            }
-            return response;
-        }
+        public Task<IActionResult> Get(int basketId) => Handle(new GetBasket.Request{Id = basketId});
     }
 }
