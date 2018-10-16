0a54873a6f77d054ad9c8b4156ea62d9bd9f5ce7
mladen <mladen.knezovic.1993@gmail.com>
2018-10-16 20:33:47 +0200

    moved query logic from GetBasket.Handler to BasketQueries.GetBasketWith...

---
+++ WebShop.Queries/BasketQueries.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebShop.Baskets;
+using WebShop.Discounts;
+
+namespace WebShop.Queries
+{
+    public class BasketQueries
+    {
+        private readonly IQueryable<Basket> _basketStore;
+        private readonly IQueryable<Discount> _discountStore;
+        private readonly decimal _maxAllowedDiscount;
+
+        public BasketQueries(IQueryable<Basket> basketStore, IQueryable<Discount> discountStore, decimal maxAllowedDiscount)
+        {
+            _basketStore = basketStore;
+            _discountStore = discountStore;
+            _maxAllowedDiscount = maxAllowedDiscount;
+        }
+
+        public async Task<Basket> GetBasketWithDiscountsApplied(int basketId)
+        {
+            var basket = await _basketStore
+                .Where(b => b.Id == basketId)
+                .Include(b => b.Items)
+                    .ThenInclude(i => i.Product)
+                .FirstOrDefaultAsync();
+
+            if (basket == null)
+                return null;
+
+            basket.TotalPrice = basket.Items.Select(i => i.Price).Sum();
+
+            foreach (var basketItem in basket.Items)
+            {
+                basketItem.Discounts = await GetDiscountsFor(basketItem);
+                CalculateItemPrice(basketItem);
+            }
+
+            return basket;
+        }
+
+        private void CalculateItemPrice(BasketItem item)
+        {
+            var totalDiscount = item.Discounts.Select(d => d.Value).Sum();
+            if (totalDiscount > _maxAllowedDiscount)
+                totalDiscount = _maxAllowedDiscount;
+
+            var without = item.Product.RegularPrice * totalDiscount;
+            item.Price = item.Product.RegularPrice - without;
+        }
+
+        private Task<List<Discount>> GetDiscountsFor(BasketItem item)
+        {
+            var numberOfProductsInBasket = item.Basket.Items.Count(i => i.ProductId == item.ProductId);
+            return _discountStore
+                .Where(d => d.ForProductId == item.ProductId &&
+                            numberOfProductsInBasket >= d.RequiredMinimalQuantity)
+                .ToListAsync();
+        }
+    }
+}
--- WebShop.UseCases/GetBasket.cs
+++ WebShop.UseCases/GetBasket.cs
@@ -1,11 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ApplicationKernel.Domain.MediatorSystem;
+﻿using ApplicationKernel.Domain.MediatorSystem;
 using FluentValidation;
-using Microsoft.EntityFrameworkCore;
 using WebShop.Baskets;
-using WebShop.Discounts;
 
 namespace WebShop.Features
 {
@@ -26,48 +21,15 @@ namespace WebShop.Features
 
         public class Handler : RequestHandler<Request>
         {
-            public Handler(IQueryable<Basket> baskets, IQueryable<Discount> discountStore, decimal maxAllowedDiscount)
+            public Handler(GetBasketWithDiscountsApplied get)
             {
                 HandleWith(async (request, token) =>
                 {
-                    var basket = await baskets
-                        .Where(b => b.Id == request.Id)
-                        .Include(b => b.Items)
-                            .ThenInclude(i => i.Product)
-                        .FirstOrDefaultAsync();
-
-                    if (basket == null)
-                        return Responses.Failure("Basket not found");
-
-                    basket.TotalPrice = basket.Items.Select(i => i.Price).Sum();
-                    
-                    foreach (var basketItem in basket.Items)
-                    {
-                        basketItem.Discounts = await GetDiscountsFor(basketItem);
-                        CalculateItemPrice(basketItem);
-                    }
-
-                    return Responses.Success(basket);
+                    var basket = await get(request.Id);
+                    return basket == null ?
+                        Responses.Failure("Basket not found") :
+                        Responses.Success(basket);
                 });
-
-                void CalculateItemPrice(BasketItem item)
-                {
-                    var totalDiscount = item.Discounts.Select(d => d.Value).Sum();
-                    if (totalDiscount > maxAllowedDiscount)
-                        totalDiscount = maxAllowedDiscount;
-
-                    var without = item.Product.RegularPrice * totalDiscount;
-                    item.Price = item.Product.RegularPrice - without;
-                }
-
-                Task<List<Discount>> GetDiscountsFor(BasketItem item)
-                {
-                    var numberOfProductsInBasket = item.Basket.Items.Count(i => i.ProductId == item.ProductId);
-                    return discountStore
-                        .Where(d => d.ForProductId == item.ProductId &&
-                                    numberOfProductsInBasket >= d.RequiredMinimalQuantity)
-                        .ToListAsync();
-                }
             }
         }
     }
