5e260bd4b739c87ee004ed25a9ea816ec1102561
mladen <mladen.knezovic.1993@gmail.com>
2018-10-19 01:58:06 +0200

    added utilities

--- Utilities/Utilities.cs
+++ Utilities/Utilities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Utilities
@@ -9,11 +10,47 @@ namespace Utilities
         public static void ForEach<T>(this IEnumerable<T> values, Action<T> action)
         {
             foreach (var value in values)
-                action(value);
+                   action(value);
+        }
+        
+        public static decimal PercentOf(this decimal percentage, decimal valueToBeDiscounted)
+        {
+            var dec = percentage / 100;
+            var without = valueToBeDiscounted * dec;
+            return valueToBeDiscounted - without;
+        }
+
+        public static decimal PercentOf(this int percentage, decimal valueToBeDiscounted)
+        {
+            return PercentOf((decimal)percentage, valueToBeDiscounted);
         }
 
         public static Task WhenAll(this IEnumerable<Task> tasks) => Task.WhenAll(tasks);
 
         public static MustAssertions Must<T>(this T o) => new MustAssertions(o);
+    }
+
+    public static class Collections
+    {
+        public static bool ContainsN<T>(this IEnumerable<T> enumerable, Func<T, bool> func, int count)
+        {
+            return enumerable.Count(func) == count;
+        }
+
+        public static bool ContainsOne<T>(this IEnumerable<T> enumerable, Func<T, bool> func)
+        {
+            return enumerable.ContainsN(func, 1);
+        }
+
+        public static IEnumerable<T> New<T>(Func<T> get, int count)
+        {
+            return Enumerable.Range(0, count)
+                .Select(it => get());
+        }
+
+        public static IEnumerable<T> Concat<T>(params IEnumerable<T>[] collections)
+        {
+            return collections.Skip(1).Aggregate(collections.First(), (result, element) => result.Concat(element));
+        }
     }
 }
