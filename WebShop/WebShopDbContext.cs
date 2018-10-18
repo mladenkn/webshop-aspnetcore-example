1e15714b6fb49704680655bd6b7a0c8a33256802
mladen <mladen.knezovic.1993@gmail.com>
2018-10-19 01:58:36 +0200

    added db context

---
+++ WebShop/WebShopDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebShop.Baskets;
+using WebShop.Discounts;
+
+namespace WebShop
+{
+    public class WebShopDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+
+        public WebShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BasketItem>().Ignore(it => it.Discounts);
+            modelBuilder.Entity<BasketItem>().Ignore(it => it.Price);
+            modelBuilder.Entity<Basket>().Ignore(it => it.TotalPrice);
+        }
+    }
+}
