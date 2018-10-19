25e13a818c06aefebbe9c2f8f0097c4f06bb4491
mladen <mladen.knezovic.1993@gmail.com>
2018-10-19 04:40:16 +0200

    beautified test code

--- WebShop.Tests/GetBasketWithDiscountsAppliedTest.cs
+++ WebShop.Tests/GetBasketWithDiscountsAppliedTest.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
-using FluentAssertions.Collections;
 using Utilities;
 using WebShop.Baskets;
 using WebShop.Discounts;
@@ -14,79 +12,15 @@ namespace WebShop.Tests
     public class GetBasketWithDiscountsAppliedTest
     {
         private readonly Fixture _fixture = new Fixture();
-        private readonly DataGenerator _data = new DataGenerator();
-
-        public GetBasketWithDiscountsAppliedTest()
-        {
-            _fixture.WithBasket(_data.Basket);
-        }
-
-        //[Fact]
-        //public async Task Buy_1_milk____get_1_milk_discounted()
-        //{
-        //    var discount = new Discount
-        //    {
-        //        TargetProductId = _data.Milk.Id,
-        //        RequiredProductId = _data.Milk.Id,
-        //        RequiredProductQuantity = 1,
-        //        TargetProductQuantity = int.MaxValue
-        //    };
-
-        //    var basketItem = _data.BasketItem(_data.Milk);
-
-        //    await _fixture
-        //        .WithProducts(_data.Milk)
-        //        .WithBasketItems(basketItem)
-        //        .WithDiscounts(discount)
-        //        .BasketItemsShould(_ =>
-        //        {
-        //            _.ContainSingle(bi => bi.Discounts.ContainsOne(d => d.Id == discount.Id) &&
-        //                                  bi.ProductId == _data.Milk.Id);
-        //        })
-        //        .Run();
-        //}
-
-        //[Fact]
-        //public async Task Buy_2_butters_and_2_milks___get_1_milk_discounted()
-        //{
-        //    var basketItems = Collections.Concat(
-        //        Collections.New(() => _data.BasketItem(_data.Butter), 2),
-        //        Collections.New(() => _data.BasketItem(_data.Milk), 2)
-        //    );
-            
-        //    var discount = new Discount
-        //    {
-        //        TargetProductId = _data.Milk.Id,
-        //        RequiredProductId = _data.Butter.Id,
-        //        RequiredProductQuantity = 2,
-        //        TargetProductQuantity = 1
-        //    };
-
-        //    await _fixture
-        //        .WithProducts(_data.Butter, _data.Milk)
-        //        .WithBasketItems(basketItems)
-        //        .WithDiscounts(discount)
-        //        .BasketItemsShould(_ =>
-        //        {
-        //            _.ContainSingle(bi => bi.Discounts.ContainsOne(d => d.Id == discount.Id) &&
-        //                                  bi.ProductId == _data.Milk.Id);
-        //        })
-        //        .Run();
-        //}
+        private readonly DataContainer _data = new DataContainer();
 
         [Fact]
         public async Task Basket_has___1_bread_1_butter_1__milk()
         {
-            var basketItems = new[]
-            {
-                _data.BasketItem(_data.Bread),
-                _data.BasketItem(_data.Butter),
-                _data.BasketItem(_data.Milk)
-            };
-
             await _fixture
-                .WithProducts(_data.Bread, _data.Bread, _data.Milk)
-                .WithBasketItems(basketItems)
+                .WithBasketItemOf(_data.Butter)
+                .WithBasketItemOf(_data.Bread)
+                .WithBasketItemOf(_data.Milk)
                 .BasketPriceShouldBe(2.95m)
                 .Run();
         }
@@ -94,18 +28,10 @@ namespace WebShop.Tests
         [Fact]
         public async Task Basket_has___2_butters_2_breads()
         {
-            var basketItems = new[]
-            {
-                _data.BasketItem(_data.Bread),
-                _data.BasketItem(_data.Bread),
-                _data.BasketItem(_data.Butter),
-                _data.BasketItem(_data.Butter),
-            };
-
             await _fixture
-                .WithProducts(_data.Bread, _data.Butter)
                 .WithDiscounts(_data.BreadDiscount)
-                .WithBasketItems(basketItems)
+                .WithBasketItemsOf(_data.Butter, 2)
+                .WithBasketItemsOf(_data.Bread, 2)
                 .BasketPriceShouldBe(3.1m)
                 .Run();
         }
@@ -113,18 +39,9 @@ namespace WebShop.Tests
         [Fact]
         public async Task Basket_has___4_milks()
         {
-            var basketItems = new[]
-            {
-                _data.BasketItem(_data.Milk),
-                _data.BasketItem(_data.Milk),
-                _data.BasketItem(_data.Milk),
-                _data.BasketItem(_data.Milk),
-            };
-
             await _fixture
-                .WithProducts(_data.Milk)
                 .WithDiscounts(_data.MilkDiscount)
-                .WithBasketItems(basketItems)
+                .WithBasketItemsOf(_data.Milk, 4)
                 .BasketPriceShouldBe(3.45m)
                 .Run();
         }
@@ -132,25 +49,11 @@ namespace WebShop.Tests
         [Fact]
         public async Task Basket_has___2_butter_1_bread_8_milk()
         {
-            var basketItems = new[]
-            {
-                _data.BasketItem(_data.Butter),
-                _data.BasketItem(_data.Butter),
-                _data.BasketItem(_data.Bread),
-                _data.BasketItem(_data.Milk),
-                _data.BasketItem(_data.Milk),
-                _data.BasketItem(_data.Milk),
-                _data.BasketItem(_data.Milk),
-                _data.BasketItem(_data.Milk),
-                _data.BasketItem(_data.Milk),
-                _data.BasketItem(_data.Milk),
-                _data.BasketItem(_data.Milk),
-            };
-
             await _fixture
-                .WithProducts(_data.Milk, _data.Bread, _data.Butter)
                 .WithDiscounts(_data.MilkDiscount, _data.BreadDiscount)
-                .WithBasketItems(basketItems)
+                .WithBasketItemsOf(_data.Butter, 2)
+                .WithBasketItemOf(_data.Bread)
+                .WithBasketItemsOf(_data.Milk, 8)
                 .BasketPriceShouldBe(9)
                 .Run();
         }
@@ -158,24 +61,10 @@ namespace WebShop.Tests
         private class Fixture
         {
             private readonly WebShopDbContext _db = TestServiceFactory.Database();
-
-            private readonly List<Product> _products = new List<Product>();
+            private readonly DataContainer _data = new DataContainer();
             private readonly List<Discount> _discounts = new List<Discount>();
             private readonly List<BasketItem> _basketItems = new List<BasketItem>();
-            private Basket _basket;
             private decimal _expectedBasketPrice;
-
-            public Fixture WithBasket(Basket basket)
-            {
-                _basket = basket;
-                return this;
-            }
-
-            public Fixture WithProducts(params Product[] products)
-            {
-                _products.AddRange(products);
-                return this;
-            }
 
             public Fixture WithDiscounts(params Discount[] discounts)
             {
@@ -183,9 +72,16 @@ namespace WebShop.Tests
                 return this;
             }
 
-            public Fixture WithBasketItems(params BasketItem[] basketItems)
+            public Fixture WithBasketItemsOf(Product product, int count)
             {
+                var basketItems = Collections.New(() => _data.BasketItem(product), count);
                 _basketItems.AddRange(basketItems);
+                return this;
+            }
+
+            public Fixture WithBasketItemOf(Product product)
+            {
+                _basketItems.Add(_data.BasketItem(product));
                 return this;
             }
 
@@ -197,21 +93,21 @@ namespace WebShop.Tests
 
             public async Task Run()
             {
-                _db.AddRange(_basket);
-                _db.AddRange(_products);
+                var basket = _data.Basket;
+                _db.AddRange(basket);
                 _db.AddRange(_discounts);
                 _db.AddRange(_basketItems);
                 _db.SaveChanges();
 
                 var sut = new BasketQueries(_db.Baskets, _db.Discounts, 1);
-                var basket = await sut.GetBasketWithDiscountsApplied(_basket.Id);
-                basket.Should().NotBeNull();
-                basket.Id.Should().Be(_basket.Id);
-                basket.TotalPrice.Should().Be(_expectedBasketPrice);
+                var returnedBasket = await sut.GetBasketWithDiscountsApplied(basket.Id);
+                returnedBasket.Should().NotBeNull();
+                returnedBasket.Id.Should().Be(returnedBasket.Id);
+                returnedBasket.TotalPrice.Should().Be(_expectedBasketPrice);
             }
         }
 
-        private class DataGenerator
+        private class DataContainer
         {
             public Basket Basket { get; } = new Basket { Id = 1 };
 
@@ -236,7 +132,7 @@ namespace WebShop.Tests
             public Discount BreadDiscount { get; }
             public Discount MilkDiscount { get; }
 
-            public DataGenerator()
+            public DataContainer()
             {
                 MilkDiscount = new Discount
                 {
