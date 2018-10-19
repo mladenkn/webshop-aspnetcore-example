c8f37e4356e553e7345fe861d6b257903720b5d0
mladen <mladen.knezovic.1993@gmail.com>
2018-10-19 23:11:40 +0200

    minor

--- WebShop.Infrastructure/OnBasketWithPriceRequested.cs
+++ WebShop.Infrastructure/OnBasketWithPriceRequested.cs
@@ -1,18 +1,26 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using ApplicationKernel.Domain.MediatorSystem;
 using MediatR.Pipeline;
+using Utilities;
 using WebShop.Baskets;
 using WebShop.Features;
 
 namespace WebShop.Infrastructure
 {
-    // insted of using BasketSumCalculatedEvent 
+    // instead of using BasketSumCalculatedEvent 
     // this is invoked by mediator after processing GetBasket.Request
     public class OnBasketWithPriceRequested: IRequestPostProcessor<GetBasket.Request, Response<Basket>>
     {
         public Task Process(GetBasket.Request request, Response<Basket> response)
         {
+            var basket = response.Payload;
+
+            basket.Items.Must().NotBeNull();
+            basket.Items.First().Discounts.Must().NotBeNull();
+
             // TODO: log
+
             throw new System.NotImplementedException();
         }
     }
