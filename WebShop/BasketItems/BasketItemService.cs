48020963680000f961662c474c4bb039128e428b
mladen <mladen.knezovic.1993@gmail.com>
2018-10-19 22:28:44 +0200

    decoupled into more services

---
+++ WebShop/BasketItems/BasketItemService.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using WebShop.Baskets;
+
+namespace WebShop.BasketItems
+{
+    public delegate decimal CalculateBasketItemPrice(BasketItem basketItem);
+
+    public class BasketItemService
+    {
+        private readonly decimal _maxAllowedDiscount;
+
+        public BasketItemService(decimal maxAllowedDiscount)
+        {
+            _maxAllowedDiscount = maxAllowedDiscount;
+        }
+
+        public decimal CalculateItemPrice(BasketItem item)
+        {
+            var totalDiscount = item.Discounts.Select(d => d.Value).Sum();
+            if (totalDiscount > _maxAllowedDiscount)
+                totalDiscount = _maxAllowedDiscount;
+
+            var without = item.Product.RegularPrice * totalDiscount;
+            item.Price = item.Product.RegularPrice - without;
+
+            return item.Price;
+        }
+    }
+}
---
+++ WebShop/Baskets/BasketService.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using ApplicationKernel.Domain.MediatorSystem;
+using Microsoft.EntityFrameworkCore;
+using Utilities;
+using WebShop.BasketItems;
+using WebShop.Discounts;
+
+namespace WebShop.Baskets
+{
+    public delegate Task<Basket> GetBasketWithDiscountsApplied(int basketId);
+
+    public class BasketService
+    {
+        private readonly IQueryable<Basket> _basketStore;
+        private readonly IQueryable<Discount> _discountStore;
+        private readonly IMediator _mediator;
+        private readonly GetDiscountsFor _getDiscountsFor;
+        private readonly CalculateBasketItemPrice _calculateBasketItemPrice;
+
+        public BasketService(
+            IQueryable<Basket> basketStore, 
+            IQueryable<Discount> discountStore, 
+            IMediator mediator,
+            GetDiscountsFor getDiscountsFor,
+            CalculateBasketItemPrice calculateBasketItemPrice)
+        {
+            _basketStore = basketStore;
+            _discountStore = discountStore;
+            _mediator = mediator;
+            _getDiscountsFor = getDiscountsFor;
+            _calculateBasketItemPrice = calculateBasketItemPrice;
+        }
+
+        public async Task<Basket> GetBasketWithDiscountsApplied(int basketId)
+        {
+            var basket = await _basketStore
+                .Where(b => b.Id == basketId)
+                .Include(b => b.Items)
+                    .ThenInclude(i => i.Product)
+                .FirstOrDefaultAsync();
+
+            if (basket == null)
+                return null;
+
+            var allDiscounts = await _discountStore.ToListAsync();
+
+            var grantedDiscounts = new List<DiscountGranted>();
+            foreach (var basketItem in basket.Items)
+            {
+                basketItem.Discounts = _getDiscountsFor(basketItem, allDiscounts, grantedDiscounts);
+                _calculateBasketItemPrice(basketItem);
+            }
+
+            basket.TotalPrice = basket.Items.Select(i => i.Price).Sum();
+
+            await MediatorExtensions.Publish(_mediator, new BasketSumCalculatedEvent(basket));
+
+            // why doesn't this work?
+            //await _mediator.Publish(new BasketSumCalculatedEvent(basket));
+
+            return basket;
+        }
+    }
+}
