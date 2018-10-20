2b4f2581605993637a87042cc2e10e929deca603
mladen <mladen.knezovic.1993@gmail.com>
2018-10-20 19:55:28 +0200

    put all read store queries into one file

--- WebShop.Infrastructure.ReadStore/Queries.cs
+++ WebShop.Infrastructure.ReadStore/Queries.cs
@@ -1,35 +1,31 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using WebShop.Baskets;
 using WebShop.Infrastructure.ReadStore.Refreshing;
 
-namespace WebShop.Infrastructure.ReadStore.Queries
+namespace WebShop.Infrastructure.ReadStore
 {
-    public class BasketQueries
+    public class Queries
     {
         private readonly IDataRefreshJobsQueue _jobs;
+        private readonly IReadStore _readStore;
 
-        public BasketQueries(IDataRefreshJobsQueue jobs)
+        public Queries(IDataRefreshJobsQueue jobs, IReadStore readStore)
         {
             _jobs = jobs;
+            _readStore = readStore;
         }
 
         public async Task<Basket> GetBasketWithDiscountsApplied(int basketId)
         {
-            Task<Basket> GetActual()
-            {
-                throw new NotImplementedException();
-            }
-
             var job = _jobs.Current.OfType<RefreshBasketWithItemJob>().FirstOrDefault(j => j.BasketId == basketId);
 
             if (job == null)
-                return await GetActual();
+                return await _readStore.GetBasket(basketId);
             else
             {
                 await job.Task;
-                return await GetActual();
+                return await _readStore.GetBasket(basketId);
             }
         }
     }
