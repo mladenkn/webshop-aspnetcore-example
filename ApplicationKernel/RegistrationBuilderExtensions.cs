89cdb72bbb257031eafd8b308ea0a1b25311aa09
mladen <mladen.knezovic.1993@gmail.com>
2018-10-21 17:21:35 +0200

    added RegisterDelegate autofac extension and tests for it

---
+++ ApplicationKernel/RegistrationBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Autofac;
+
+namespace ApplicationKernel
+{
+    public static class RegistrationBuilderExtensions
+    {
+        public static ContainerBuilder RegisterDelegate<T, TDelegate>(this ContainerBuilder container, Func<T, TDelegate> getDelegate)
+        {
+            container.RegisterType<T>();
+            container.Register(c => getDelegate(c.Resolve<T>()));
+            return container;
+        }
+    }
+}
