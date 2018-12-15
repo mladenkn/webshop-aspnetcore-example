85efb68b5524229e4bb2bace4c278b49d7322d64
Mladen <mladsdf@sdf.com>
2018-12-15 19:27:23 +0100

    cleenup

--- Utilities/Utilities.cs
+++ Utilities/Utilities.cs
@@ -6,18 +6,6 @@ namespace Utilities
 {
     public static class Collections
     {
-        public static bool ContainsN<T>(this IEnumerable<T> enumerable, Func<T, bool> func, int count)
-        {
-            return enumerable.Count(func) == count;
-        }
-
-        public static IEnumerable<T> New<T>(Func<T> get, int count)
-        {
-            return Enumerable
-                .Range(0, count)
-                .Select(it => get());
-        }
-
         public static MustAssertions Must<T>(this T o) => new MustAssertions(o);
     }
 }
--- WebShop.Infrastructure/SmartQueries.cs
+++ WebShop.Infrastructure/SmartQueries.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using WebShop.Models;
@@ -31,21 +30,6 @@ namespace WebShop.DataAccess
                 .Where(basketContainsSomeTargetProducts)
                 .ToListAsync();
 
-            return r;
-        }
-
-        public async Task<IEnumerable<Discount>> GetDiscountsFor(int basketId, BasketItem notPersistedBasketItem)
-        {
-            Expression<Func<Discount, bool>> basketContainsProductInRequiredQuantity =
-                d => _db.BasketItems.Count(b => b.BasketId == basketId && b.ProductId == d.RequiredProductId) >= d.RequiredProductRequiredQuantity;
-
-            Expression<Func<Discount, bool>> basketContainsSomeTargetProducts =
-                d => _db.BasketItems.Any(bi => bi.BasketId == basketId && bi.ProductId == d.TargetProductId);
-
-            var r = await _db.Discounts
-                .Where(basketContainsProductInRequiredQuantity)
-                .Where(basketContainsSomeTargetProducts)
-                .ToListAsync();
             return r;
         }
     }
