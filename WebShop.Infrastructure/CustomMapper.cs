6f5d68013c6a04ad921f0bd000c167214cb35686
Mladen <mladsdf@sdf.com>
2019-03-20 14:17:09 +0100

    added CustomMapper - mappings for Discount

---
+++ WebShop.Infrastructure/CustomMapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using WebShop.Models;
+
+namespace WebShop.Infrastructure.DataAccess
+{
+    public interface ICustomMapper
+    {
+        (IEnumerable<RequiredProductOfDiscount> requiredProducts, IEnumerable<MicroDiscount> microDiscounts) ToDbModels(Discount domainModel);
+        Discount ToDomainModel(IEnumerable<RequiredProductOfDiscount> requiredProducts, IEnumerable<MicroDiscount> microDiscounts);
+    }
+
+    public class CustomMapper : ICustomMapper
+    {
+        private readonly IMapper _mapper;
+
+        public CustomMapper(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public Discount ToDomainModel(IEnumerable<RequiredProductOfDiscount> requiredProducts,
+            IEnumerable<MicroDiscount> microDiscounts)
+        {
+            var requiredProducts2 = requiredProducts.Select(m => _mapper.Map<Discount.RequiredProduct>(m)).ToList();
+            var microDiscounts2 = microDiscounts.Select(m => _mapper.Map<Discount.MicroDiscount>(m)).ToList();
+            var id = requiredProducts.First().DiscountId;
+            return new Discount(id, requiredProducts2, microDiscounts2);
+        }
+
+        public (IEnumerable<RequiredProductOfDiscount> requiredProducts,
+            IEnumerable<MicroDiscount> microDiscounts) ToDbModels(Discount domainModel)
+        {
+            var requiredProducts2 = domainModel.RequiredProducts.Select(m => _mapper.Map<RequiredProductOfDiscount>(m));
+            var microDiscounts2 = domainModel.MicroDiscounts.Select(m => _mapper.Map<MicroDiscount>(m));
+            return (requiredProducts2, microDiscounts2);
+        }
+    }
+}
--- WebShop/Models/Discount.cs
+++ WebShop/Models/Discount.cs
@@ -17,9 +17,23 @@ namespace WebShop.Models
             public decimal Value { get; set; }
         }
 
+        public Discount(int id, List<RequiredProduct> requiredProducts, List<MicroDiscount> microDiscounts)
+        {
+            Id = id;
+            RequiredProducts = requiredProducts;
+            MicroDiscounts = microDiscounts;
+        }
+
+        public Discount(int id)
+        {
+            Id = id;
+            RequiredProducts = new List<RequiredProduct>();
+            MicroDiscounts = new List<MicroDiscount>();
+        }
+
         public int Id { get; set; }
-        public List<RequiredProduct> RequiredProducts { get; set; }
-        public List<MicroDiscount> MicroDiscounts { get; set; }
+        public List<RequiredProduct> RequiredProducts { get; }
+        public List<MicroDiscount> MicroDiscounts { get; }
 
         public void AddRequiredProduct(int productId, int requiredQuantity)
         {
